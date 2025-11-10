7f72c9a29ed0ce183cfae46ddcbffaa7e9f922c8
muyouming <muyouming@gmail.com>
2025-11-10 23:02:51 +0900

    Add static library support for all products

--- Package.swift
+++ Package.swift
@@ -10,9 +10,7 @@ let package = Package(
   ],
   products: [
     .library(
-      name: "MisakiSwift",
-      type: .dynamic,
-      targets: ["MisakiSwift"]
+      name: "MisakiSwift", type: .static, targets: ["MisakiSwift"]
     ),
   ],
   dependencies: [
